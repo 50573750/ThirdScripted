6271285c75f27a5e53355bce957215dd3a8c8545
Bookman <Bookman@vip.163.com>
2015-07-17 22:57:45 +0800

    Initial Commit

---
+++ Experiments/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Experiments
+//
+//  Created by BookmanXiaohan on 15/7/17.
+//  Copyright (c) 2015年 BookmanXiaohan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
