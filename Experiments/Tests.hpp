8fa60f1805fac08e16ea814b409e54bca4937d6e
BookmanHan <50573750@163.com>
2015-07-29 22:03:04 +0800

    To Refine HMM.

--- Experiments/Tests.hpp
+++ Experiments/Tests.hpp
@@ -3,6 +3,7 @@
 
 #include "PPM.hpp"
 #include "IsingModel.hpp"
+#include "HiddenMarkovModel.hpp"
 
 void draw_random_circle_ising_model()
 {
@@ -34,6 +35,19 @@ void draw_random_circle_ising_model()
     IsingModelNaiveMean imnm(image);
     imnm.run(30);
     PPM::write(imnm.result(), "/Users/Bookman/Documents/trys/draw_random_circle.ppm");
+}
+
+void test_for_hmm()
+{
+    HiddenMarkovModel           hmm(10, 4);
+    
+    vector<vector<unsigned>>    datasets = { {0,1,2,3,1,0}, {1,0,1,2,3,0}, {1,1,2,3,1,1,0} };
+    hmm.baum_welch_training(datasets, 5);
+    
+    vector<vector<unsigned>>    testcase = { {1,2,3}, {0,1,2}, {0,0,0} };
+    cout<<-log(hmm.probability(testcase[0]))<<endl;
+    cout<<-log(hmm.probability(testcase[1]))<<endl;
+    cout<<-log(hmm.probability(testcase[2]))<<endl;
 }
 
 #endif
