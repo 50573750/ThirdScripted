bd0ec9587ed7d3dbc83009aef02867fbb20c0f92
BookmanHan <50573750@163.com>
2015-08-01 22:21:05 +0800

    To Add Feed Forward Back Propagation Neural Network.

---
+++ Experiments/FeadForwardNeualNetworkBackPropergation.hpp
@@ -0,0 +1,13 @@
+//
+//  FeadForwardNeualNetworkBackPropergation.h
+//  Experiments
+//
+//  Created by BookmanXiaohan on 15/8/1.
+//  Copyright (c) 2015年 BookmanXiaohan. All rights reserved.
+//
+
+#ifndef Experiments_FeadForwardNeualNetworkBackPropergation_h
+#define Experiments_FeadForwardNeualNetworkBackPropergation_h
+
+
+#endif
--- Experiments/main.cpp
+++ Experiments/main.cpp
@@ -1,9 +1,60 @@
 #include "Library.hpp"
 #include "Tests.hpp"
 
+class FeedForwardNeualNetwokBackPropergation
+{
+protected:
+    vector<MatrixXd>    weights;
+    const unsigned      n_input;
+    unsigned            n_output;
+    
+public:
+    FeedForwardNeualNetwokBackPropergation(int n_input, int n_output)
+    :n_input(n_input), n_output(n_output)
+    {
+        weights.push_back(MatrixXd::Random(n_input, n_output));
+    }
+    
+    void add_hidden_layer(int n_nodes)
+    {
+        weights.back() = MatrixXd::Random(weights.back().rows(), n_nodes);
+        weights.push_back(MatrixXd::Random(n_nodes, n_output));
+    }
+    
+    
+    MatrixXd infer(const VectorXd& input)
+    {
+        auto sigmod = [&](MatrixXd::RowXpr& mat)
+        {
+            for(auto i=0; i<mat.rows(); ++i)
+            {
+                mat(i) = 1/(1 + exp(-mat(i)));
+            }
+        };
+        
+        auto sigmod_dervation = [&](MatrixXd::RowXpr& mat)
+        {
+            for(auto i=0; i<mat.rows(); ++i)
+            {
+                mat(i) = mat(i) * (1-mat(i));
+            }
+        };
+        
+        MatrixXd output(weights.size()+1, input.size());
+        output.row(0) = input;
+        for(auto i=0; i<weights.size(); ++i)
+        {
+            output.row(i+1) = output.row(i) * weights[i];
+            sigmod(output.row(i+1));
+        }
+        
+        return output.row(weights.size());
+    }
+};
+
 int main()
 {
     srand(time(nullptr));
-    test_for_HMM(); 
+
     return 0;
 }
